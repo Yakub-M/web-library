3e596573f31866a5b58fef3b36f793dcda897b29
Yakub <yakub2001@protonmail.com>
2025-02-11 13:06:05 +0200

    Initial commit of WebLibrary app

---
+++ src/test/java/com/yakub/weblibrary/WebLibrary.java
@@ -0,0 +1,13 @@
+package com.yakub.weblibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLibraryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
